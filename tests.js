1f8b954260b5bf37bc6b1d747f12c0009d76d439
Antonio Desroche <antdesroche@gmail.com>
2019-11-20 20:52:38 -0500

    testing of rushing score exercise

--- tests.js
+++ tests.js
@@ -2,13 +2,12 @@ var mocha = require('mocha')
 var calculateScore = require('./scoring').calculateScore
 var expect = require('chai').expect
 var { describe, it } = mocha
+var calculateRushingScore = require('./scoring').calculateRushingScore
 
 describe('Scoring', function () {
   it('returns the score for a quarterback', function () {
     var player = {
-      name: 'Patrick Mahomes',
       position: 'QB',
-      team: 'Kansas City',
       stats: {
         passing: {
           attempts: 25,
@@ -25,12 +24,27 @@ describe('Scoring', function () {
 
     expect(score).to.equal(40.72)
   })
+  it('Returns the score for rushing yards', () => {
+    var player = { 
+      position: 'RB',
+      stats: {
+        rushing: { attempts: 18, yards: 103, touchdowns: 2, fumbles: 0 },
+        receiving: { receptions: 6, yards: 37, touchdowns: 0, fumbles: 0 },
+        return: {
+          kickreturn: { returns: 0, yards: 0, touchdowns: 0, fumbles: 0 },
+          puntreturn: { returns: 0, yards: 0, touchdowns: 0, fumbles: 0 },
+        },
+        
+  },
+}
+var score = calculateScore(player)
+expect(score).to.equal(32)
+console.log(score)
+  })
 
   it('returns the score for a running back', function () {
-    var player = {
-      name: 'LeSean McCoy',
+    var player = { 
       position: 'RB',
-      team: 'Buffalo',
       stats: {
         rushing: { attempts: 18, yards: 103, touchdowns: 2, fumbles: 0 },
         receiving: { receptions: 6, yards: 37, touchdowns: 0, fumbles: 0 },
@@ -48,9 +62,7 @@ describe('Scoring', function () {
 
   it('returns the score for a receiver', function () {
     var player = {
-      name: 'Tyler Lockett',
       position: 'WR',
-      team: 'Seattle',
       stats: {
         rushing: { attempts: 0, yards: 0, touchdowns: 0, fumbles: 0 },
         receiving: { receptions: 6, yards: 91, touchdowns: 1, fumbles: 0 },
@@ -68,9 +80,7 @@ describe('Scoring', function () {
 
   it('returns the score for a tightend', function () {
     var player = {
-      name: 'Rob Gronkowski',
       position: 'TE',
-      team: 'New England',
       stats: {
         receiving: { receptions: 8, yards: 137, touchdowns: 2, fumbles: 0 },
       }
@@ -83,9 +93,7 @@ describe('Scoring', function () {
 
   it('returns 0 when position is unknown', function () {
     var player = {
-      name: 'Robbie Gould',
       position: 'K',
-      team: 'San Francisco',
       stats: {
         fieldgoals: { attempts: 3, made: 2 },
         xp: { attempts: 2, made: 2 }
